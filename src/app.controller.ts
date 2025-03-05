ce41e215a28176cee980ba572814b69d7c6c34f9
nullisdefined <jaeuu.dev@gmail.com>
2025-03-05 20:54:57 +0900

    chore: test logger 작성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Logger } from 'winston';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('winston')
+    private readonly logger: Logger,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.logger.info('Called getHello()');
+    this.logger.debug('Debug message');
+    this.logger.error('Error message');
     return this.appService.getHello();
   }
 }
