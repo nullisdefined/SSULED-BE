3b7dc92b58abe5842c4f9982fd8094549c01f655
nullisdefined <jaeuu.dev@gmail.com>
2025-03-05 20:21:41 +0900

    chore: hot reload 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,12 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  overrides: [
+    {
+      files: ['webpack.config.js'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+  ],
 };
