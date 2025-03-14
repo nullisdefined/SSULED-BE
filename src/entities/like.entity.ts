0af22ba69bdfc24096d7033e1939f65eb1dae069
nullisdefined <jaeuu.dev@gmail.com>
2025-03-14 20:09:31 +0900

    feat: like 엔티티

---
+++ src/entities/like.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('like')
+export class Like {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @Column({ name: 'user_id', type: 'int' })
+  userId: number;
+
+  @Column({ name: 'post_id', type: 'int' })
+  postId: number;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+}
