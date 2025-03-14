e06d8c51bb526211d0f9340409922924ce7b1603
nullisdefined <jaeuu.dev@gmail.com>
2025-03-14 19:59:25 +0900

    feat: post 엔티티

---
+++ src/entities/post.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('post')
+export class Post {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id: number;
+
+  @Column({ name: 'title', type: 'varchar', nullable: true })
+  title: string;
+
+  @Column({ name: 'content', type: 'varchar', nullable: true })
+  content: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: Date;
+}
