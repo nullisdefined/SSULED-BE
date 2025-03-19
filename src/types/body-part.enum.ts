9b130fac9b9a53c2d1c0e41ae2a1127f93cfd5d3
nullisdefined <jaeuu.dev@gmail.com>
2025-03-19 20:33:05 +0900

    refactor: enum 타입 추가

---
+++ src/types/body-part.enum.ts
@@ -0,0 +1,10 @@
+export enum BodyPartEnum {
+  CHEST = 'chest', // 가슴
+  BACK = 'back', // 등
+  LEGS = 'legs', // 하체
+  CORE = 'core', // 코어
+  SPORTS = 'sports', // 스포츠
+  SHOULDERS_ARMS = 'shoulders_arms', // 어깨&팔
+  CARDIO = 'cardio', // 유산소
+  OTHER = 'other', // 기타
+}
