fff01aba70f8715b79d7a869dc4a8efab0faaf4f
qowldud <bjy6631@daum.net>
2025-04-01 13:20:17 +0900

    feat: 닉네임 중복 체크

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { UserUuid } from '@/decorators/user-uuid.deorator';
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiLogout } from '@/decorators/swagger.decorator';
 
@@ -13,5 +13,14 @@ export class UsersController {
   @ApiLogout()
   async logout(@UserUuid() userUuid: string) {
     return this.usersService.logout(userUuid);
+  }
+
+  @Post('/nickname')
+  @UseGuards(JwtAuthGuard)
+  async updateNickname(
+    @Body('newNickname') newNickname: string,
+    @UserUuid() UserUuid: string,
+  ) {
+    return this.usersService.updateNickname(UserUuid, newNickname);
   }
 }
